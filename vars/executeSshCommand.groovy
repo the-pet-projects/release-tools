017a498bd1debff317e6a38cfb7b4af6d5ef7c10
joao.reis <joao.reis@farfetch.com>
2017-09-17 17:17:24 +0100

    fix?

--- vars/executeSshCommand.groovy
+++ vars/executeSshCommand.groovy
@@ -1,6 +1,6 @@
 // vars/executeSshCommand.groovy
 
 def call(String username, String password, String cmd) {
-	def sshCmd = 'sshpass -p \'${password}\' ssh ${username}@10.0.1.5 \''${cmd}'\''
+	def sshCmd = "sshpass -p '${password}' ssh ${username}@10.0.1.5 '${cmd}'"
 	sh sshCmd
 }
