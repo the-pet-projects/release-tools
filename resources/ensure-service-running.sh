609a49f89ab2086db026c0ff1dc660f415608ed1
Pedro Moreira <pedro.ms.moreira51@gmail.com>
2017-09-30 17:35:28 +0100

    add petprojects to service name

--- resources/ensure-service-running.sh
+++ resources/ensure-service-running.sh
@@ -13,5 +13,5 @@ if [ $(docker service ls -f name=${IMAGE_NAME} --quiet | wc -l) -eq 0 ]; then
         --restart-delay 5s \
         --update-delay 10s \
         --update-parallelism 1 \
-        ${IMAGE_NAME}
+        petprojects/${IMAGE_NAME}:${PIPELINE_VERSION}
 fi
