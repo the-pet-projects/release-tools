fc45c4259676c77f0c34f4a0ce32af64723e2a0e
Pedro Moreira <pedro.ms.moreira51@gmail.com>
2017-09-30 17:54:39 +0100

    add $ to pipeline version

--- resources/ensure-service-running.sh
+++ resources/ensure-service-running.sh
@@ -7,11 +7,16 @@ echo "Check service is running - ${IMAGE_NAME}";
 SERVICES=$(docker service ls -f name=${IMAGE_NAME} --quiet | wc -l)
 
 if [ $(docker service ls -f name=${IMAGE_NAME} --quiet | wc -l) -eq 0 ]; then
+
+    echo "Creating Service - ${IMAGE_NAME}";
+
     docker service create \
-        --name petprojects/${IMAGE_NAME}:{PIPELINE_VERSION} \
+        --name petprojects/${IMAGE_NAME}:${PIPELINE_VERSION} \
         --restart-condition any \
         --restart-delay 5s \
         --update-delay 10s \
         --update-parallelism 1 \
         petprojects/${IMAGE_NAME}:${PIPELINE_VERSION}
+
+    echo "SErvice is Created - ${IMAGE_NAME}"
 fi
