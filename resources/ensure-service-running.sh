485f5c32ecefe7695984341b76d4e515dc9e2b08
Pedro Moreira <pedro.ms.moreira51@gmail.com>
2017-09-30 15:51:01 +0100

    add more echo

--- resources/ensure-service-running.sh
+++ resources/ensure-service-running.sh
@@ -1,6 +1,7 @@
 #Ensure service is running
-echo "Check service is running - ${IMAGE_NAME}"
-SERVICES=$(docker service ls --f name=${IMAGE_NAME} --quiet | wc -l)
+echo "Check service is running - ${IMAGE_NAME}";
+SERVICES=$(docker service ls --f name=${IMAGE_NAME} --quiet | wc -l);
+echo "Service count - $SERVICES";
 if [[ "$SERVICES" -eq 0]]; then
     docker service create \
         --name ${IMAGE_NAME} \
