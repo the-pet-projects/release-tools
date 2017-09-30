da34f0b73642976698f4ed3dccedb37063527a1b
Pedro Moreira <pedro.ms.moreira51@gmail.com>
2017-09-30 21:02:46 +0100

    add more control echo to test scripts

--- resources/ensure-service-running.sh
+++ resources/ensure-service-running.sh
@@ -6,7 +6,7 @@ SERVICE_NAME="petprojects/${IMAGE_NAME}:${PIPELINE_VERSION}";
 
 echo "Check service is running - ${IMAGE_NAME}";
 
-SERVICES=$(docker service ls -f name=${$SERVICE_NAME} --quiet | wc -l)
+SERVICES=$(docker service ls -f name=$SERVICE_NAME --quiet | wc -l)
 
 echo "SERVICES VALUE Should be 0. Actual Value = $SERVICES";
 
@@ -22,5 +22,11 @@ if [ $SERVICES -eq 0 ]; then
         --update-parallelism 1 \
         $CONTAINER_NAME
 
-    echo "Service is Created - $SERVICE_NAME"
+    SERVICES_AFTER_CREATION=$(docker service ls -f name=$SERVICE_NAME --quiet | wc -l)
+    if [ $SERVICES_AFTER_CREATION -eq 1]; then
+        echo "Service is Created - $SERVICE_NAME"
+    else
+        echo "SERVICES VALUE Should be 0. Actual Value = $SERVICES_AFTER_CREATION";
+        echo "Service is NOT Created - $SERVICE_NAME"
+    fi
 fi
