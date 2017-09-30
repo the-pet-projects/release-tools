e46830d88fc949e702f41b7b90a7a9877b9a93b4
Pedro Moreira <pedro.ms.moreira51@gmail.com>
2017-09-30 20:53:31 +0100

    add more echo and add SERVICES to if

--- resources/ensure-service-running.sh
+++ resources/ensure-service-running.sh
@@ -2,15 +2,15 @@
 
 #Ensure service is running
 
-echo "Check service is running - ${IMAGE_NAME}";
-
 SERVICE_NAME="petprojects/${IMAGE_NAME}:${PIPELINE_VERSION}";
 
-echo "SERVICE_NAME - $SERVICE_NAME";
+echo "Check service is running - ${IMAGE_NAME}";
 
-SERVICES=$(docker service ls -f name=${IMAGE_NAME} --quiet | wc -l)
+SERVICES=$(docker service ls -f name=${$SERVICE_NAME} --quiet | wc -l)
 
-if [ $(docker service ls -f name=$SERVICE_NAME --quiet | wc -l) -eq 0 ]; then
+echo "SERVICES VALUE Should be 0. Actual Value = $SERVICES";
+
+if [ $SERVICES -eq 0 ]; then
 
     echo "Creating Service - $SERVICE_NAME";
 
