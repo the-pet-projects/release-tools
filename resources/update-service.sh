38f6d915ef4f2f9aecead0560cb62611e9550565
Pedro Moreira <pedromsmoreira51@gmail.com>
2017-10-01 20:18:13 +0100

    remove --publish from docker service update

--- resources/update-service.sh
+++ resources/update-service.sh
@@ -4,7 +4,7 @@ SERVICE_IMAGE_TAG="petprojects/${IMAGE_NAME}:${PIPELINE_VERSION}";
 
 SERVICES=$(docker service ls -f name=${IMAGE_NAME} --quiet | wc -l)
 
-echo "SERVICES VALUE Should be 1. Actual Value = $SERVICES | PORT : ${PORT}";
+echo "SERVICES VALUE Should be 1. Actual Value = $SERVICES";
 
 # Update service
 if [ $SERVICES -eq 1 ]; then
@@ -14,7 +14,6 @@ if [ $SERVICES -eq 1 ]; then
         --restart-delay 5s \
         --update-delay 10s \
         --update-parallelism 1 \
-        --publish ${PORT}:80 \
         ${IMAGE_NAME}
 else
     echo "Service is NOT Created - ${IMAGE_NAME} thus WILL NOT BE UPDATED";
