7ba98d7b13f33da36a15eeea712466c6e9eb0a16
joao.reis <joao.reis@farfetch.com>
2017-11-02 21:22:37 +0000

    fixed push packages script

--- resources/push-packages.sh
+++ resources/push-packages.sh
@@ -1,7 +1,7 @@
 failureCode=0
 for line in $(find -name '*.nupkg'); 
 do 
-	dotnet nuget push $line -k ${NUGET_API_KEY} --symbol-api-key ${NUGET_API_KEY};
+	dotnet nuget push $line -k ${NUGET_API_KEY} --symbol-api-key ${NUGET_API_KEY} -s https://www.nuget.org/api/v2/package --symbols-source https://nuget.smbsrc.net/
 	lastExitCode=$?;
     	echo "lastexitcode=$lastExitCode";
     	if [ $lastExitCode != 0 ] ; then
