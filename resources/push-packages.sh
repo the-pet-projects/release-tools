c699947a5d403a7d12347aa6c31193ca59d86e4e
joao.reis <joao.reis@farfetch.com>
2017-11-02 21:54:47 +0000

    changes push packages script

--- resources/push-packages.sh
+++ resources/push-packages.sh
@@ -1,8 +1,7 @@
 failureCode=0
 for line in $(find -name '*.nupkg' | grep -G '^((?!\.symbols\.nupkg).)*$'); 
 do 
-	echo "${NUGET_API_KEY}"
-	dotnet nuget push $line -k ${NUGET_API_KEY} -s https://www.nuget.org/api/v2/package
+	dotnet nuget push $line -k "${NUGET_API_KEY}" -sk "${NUGET_API_KEY}" -s "https://www.nuget.org/api/v2/package" -ss "https://nuget.smbsrc.net/"
 	lastExitCode=$?;
     	echo "lastexitcode=$lastExitCode";
     	if [ $lastExitCode != 0 ] ; then
@@ -14,18 +13,17 @@ if [ $failureCode != 0 ] ; then
 	echo "exiting with status code $failureCode";
 	exit $failureCode;
 fi;
-for line in $(find -name '*.symbols.nupkg'); 
-do 
-	echo "${NUGET_API_KEY}"
-	dotnet nuget push $line -k ${NUGET_API_KEY} -s https://nuget.smbsrc.net/
-	lastExitCode=$?;
-    	echo "lastexitcode=$lastExitCode";
-    	if [ $lastExitCode != 0 ] ; then
-		echo "setting failurecode $lastExitCode";
-    		failureCode=$lastExitCode;
-    	fi; 
-done;
-if [ $failureCode != 0 ] ; then
-	echo "exiting with status code $failureCode";
-	exit $failureCode;
-fi;
+#for line in $(find -name '*.symbols.nupkg'); 
+#do 
+#	dotnet nuget push $line -k "${NUGET_API_KEY}" -sk "${NUGET_API_KEY}" -s "https://www.nuget.org/api/v2/package" -ss "https://nuget.smbsrc.net/"
+#	lastExitCode=$?;
+#    	echo "lastexitcode=$lastExitCode";
+#    	if [ $lastExitCode != 0 ] ; then
+#		echo "setting failurecode $lastExitCode";
+#    		failureCode=$lastExitCode;
+#    	fi; 
+#done;
+#if [ $failureCode != 0 ] ; then
+#	echo "exiting with status code $failureCode";
+#	exit $failureCode;
+#fi;
