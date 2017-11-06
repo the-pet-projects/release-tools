547c1443d5c3188052f6037d676e10f0823d2848
joao.reis <joao.reis@farfetch.com>
2017-11-06 21:49:45 +0000

    .

--- resources/push-packages.sh
+++ resources/push-packages.sh
@@ -1,6 +1,7 @@
 failureCode=0
 for line in $(find -name '*.nupkg' | grep -P '^((?!\.symbols\.nupkg).)*$'); 
 do 
+    echo $line + " -k \"${NUGET_API_KEY}\" -sk \"${NUGET_API_KEY}\" -s \"https://www.nuget.org/api/v2/package\" -ss \"https://nuget.smbsrc.net/\"";
 	dotnet nuget push $line -k "${NUGET_API_KEY}" -sk "${NUGET_API_KEY}" -s "https://www.nuget.org/api/v2/package" -ss "https://nuget.smbsrc.net/";
 	lastExitCode=$?;
     	echo "lastexitcode=$lastExitCode";
