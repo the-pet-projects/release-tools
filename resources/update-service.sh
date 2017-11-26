66b01da7e0097c4d5a796e689ca9d16a55155062
joao.reis <joao.reis@farfetch.com>
2017-11-26 19:56:41 +0000

    changed consul env variables

--- resources/update-service.sh
+++ resources/update-service.sh
@@ -14,8 +14,8 @@ if [ $SERVICES -eq 1 ]; then
         --restart-delay 5s \
         --update-delay 10s \
         --update-parallelism 1 \
-		--env-add CONSUL_ENVIRONMENT=${CONSUL_ENVIRONMENT} \
-		--env-add CONSUL_ADDRESS=${CONSUL_ADDRESS} \
+		--env-add MTS_APP_SETTINGS_ConsulStoreConfiguration:Environment=${CONSUL_ENVIRONMENT} \
+		--env-add MTS_APP_SETTINGS_ConsulClientConfiguration:Address=${CONSUL_ADDRESS} \
         ${IMAGE_NAME}
 else
     echo "Service is NOT Created - ${IMAGE_NAME} thus WILL NOT BE UPDATED";
